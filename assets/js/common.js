dd05a6da54e347918566e9b58bfd1d52cb8c4165
phamhoan_nuce <phamhoanxda@gmail.com>
2021-08-26 11:31:51 +0700

    fix js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,13 @@
 function escapeOutput(toOutput) {
-    if (toOutput != null) {
-        return toOutput.replace(/\&/g, '&amp;')
-            .replace(/\</g, '&lt;')
-            .replace(/\>/g, '&gt;')
-            .replace(/\"/g, '&quot;')
-            .replace(/\'/g, '&#x27')
-            .replace(/\//g, '&#x2F');
-    }
+  if (toOutput != null) {
+    return toOutput
+      .replace(/\&/g, "&amp;")
+      .replace(/\</g, "&lt;")
+      .replace(/\>/g, "&gt;")
+      .replace(/\"/g, "&quot;")
+      .replace(/\'/g, "&#x27")
+      .replace(/\//g, "&#x2F");
+  }
 }
 
 const queryString = window.location.search;
@@ -14,120 +15,121 @@ const hrefString = window.location.href;
 const urlParams = new URLSearchParams(queryString);
 
 function getAuth() {
-    return JSON.parse(sessionStorage.getItem("auth"));
+  return JSON.parse(sessionStorage.getItem("auth"));
 }
 
 function requiredLogin(pageName, premium = false) {
-    if (getAuth().state == 0) {
-        switch (pageName) {
-            case 'dashboard':
-                window.location.href = "/src/auth/login.html?backUrl=dashboard";
-                break;
-            case 'book-category':
-                window.location.href = "/src/auth/login.html?backUrl=book-category";
-                break;
-            case 'databook':
-                window.location.href = "/src/auth/login.html?backUrl=databook";
-                break;
-            default:
-                window.location.href = "/src/auth/login.html";
-        }
+  if (getAuth().state == 0) {
+    switch (pageName) {
+      case "dashboard":
+        window.location.href = "/src/auth/login.html?backUrl=dashboard";
+        break;
+      case "book-category":
+        window.location.href = "/src/auth/login.html?backUrl=book-category";
+        break;
+      case "databook":
+        window.location.href = "/src/auth/login.html?backUrl=databook";
+        break;
+      default:
+        window.location.href = "/src/auth/login.html";
     }
+  }
 
-    if (premium == true && getAuth().type != "premium") {
-        switch (pageName) {
-            case 'dashboard':
-                window.location.href = "/src/auth/login.html?backUrl=dashboard";
-                break;
-            case 'book-category':
-                window.location.href = "/src/auth/login.html?backUrl=book-category";
-                break;
-            case 'databook':
-                window.location.href = "/src/auth/login.html?backUrl=databook";
-                break;
-            default:
-                window.location.href = "/src/auth/login.html";
-        }
+  if (premium == true && getAuth().type != "premium") {
+    switch (pageName) {
+      case "dashboard":
+        window.location.href = "/src/auth/login.html?backUrl=dashboard";
+        break;
+      case "book-category":
+        window.location.href = "/src/auth/login.html?backUrl=book-category";
+        break;
+      case "databook":
+        window.location.href = "/src/auth/login.html?backUrl=databook";
+        break;
+      default:
+        window.location.href = "/src/auth/login.html";
     }
+  }
 }
 
 function userMenuBtnBehavior() {
-    $userWrap = $(".user-wrap");
-    $btn = $(`.user-menu-btn`);
-    $menu = $(`.user-menu-wrap`);
+  $userWrap = $(".user-wrap");
+  $btn = $(`.user-menu-btn`);
+  $menu = $(`.user-menu-wrap`);
 
-    $userWrap.click(function(e) {
-        e.preventDefault();
-        if ($btn.hasClass("fa-caret-up")) {
-            //close
-            $btn.removeClass("fa-caret-up");
-            $btn.addClass("fa-caret-down");
-            if (!$menu.hasClass("hidden")) {
-                $menu.addClass("hidden");
-            }
-        } else {
-            //open
-            $btn.removeClass("fa-caret-down");
-            $btn.addClass("fa-caret-up");
-            if ($menu.hasClass("hidden")) {
-                $menu.removeClass("hidden");
-            }
-        }
-        setTimeout(() => {
-            $(`.user-wrap .user-menu a`).each(function () {
-                let $this = $(this);
-                $this.click(() => {
-                    console.log($this.attr("href"));
-                    window.location = $this.attr("href");
-                })
-            })
-        }, 100)
-
-    });
-
+  $userWrap.click(function (e) {
+    e.preventDefault();
+    if ($btn.hasClass("fa-caret-up")) {
+      //close
+      $btn.removeClass("fa-caret-up");
+      $btn.addClass("fa-caret-down");
+      if (!$menu.hasClass("hidden")) {
+        $menu.addClass("hidden");
+      }
+    } else {
+      //open
+      $btn.removeClass("fa-caret-down");
+      $btn.addClass("fa-caret-up");
+      if ($menu.hasClass("hidden")) {
+        $menu.removeClass("hidden");
+      }
+    }
+    setTimeout(() => {
+      $(`.user-wrap .user-menu a`).each(function () {
+        let $this = $(this);
+        $this.click(() => {
+          console.log($this.attr("href"));
+          window.location = $this.attr("href");
+        });
+      });
+    }, 100);
+  });
 }
 
 var mouse_is_inside = false;
 
-$(document).ready(function() {
-    $("#userInfo").hover(function() {
-        mouse_is_inside = true;
-    }, function() {
-        mouse_is_inside = false;
-        $(`.user-menu-btn`).removeClass("fa-caret-up");
-        $(`.user-menu-btn`).addClass("fa-caret-down");
-        if (!$(`.user-menu-wrap`).hasClass("hidden")) {
-            $(`.user-menu-wrap`).addClass("hidden");
-        }
-    });
-
+$(document).ready(function () {
+  $("#userInfo").hover(
+    function () {
+      mouse_is_inside = true;
+    },
+    function () {
+      mouse_is_inside = false;
+      $(`.user-menu-btn`).removeClass("fa-caret-up");
+      $(`.user-menu-btn`).addClass("fa-caret-down");
+      if (!$(`.user-menu-wrap`).hasClass("hidden")) {
+        $(`.user-menu-wrap`).addClass("hidden");
+      }
+    }
+  );
 });
 
-
-$(function() {
+$(function () {
+  setTimeout(function () {
     if (getAuth().state == 1) {
-        if ($(`.nav-bar .user-wrap`).hasClass("hidden")) {
-            $(`.nav-bar .user-wrap`).removeClass("hidden");
-            $(`.nav-bar .user-wrap span.user-name`).text(getAuth().username);
-            $(`.nav-bar .user-wrap .user-info img`).attr("src", getAuth().ava);
-        }
-        if ($(`.nav-bar-menu .logout`).hasClass("hidden")) {
-            $(`.nav-bar-menu .logout`).removeClass("hidden");
-        }
-        if (!$(`.nav-bar-menu .login`).hasClass("hidden")) {
-            $(`.nav-bar-menu .login`).addClass("hidden");
-        }
+      if ($(`.nav-bar .user-wrap`).hasClass("hidden")) {
+        $(`.nav-bar .user-wrap`).removeClass("hidden");
+        $(`.nav-bar .user-wrap span.user-name`).text(getAuth().username);
+        $(`.nav-bar .user-wrap .user-info img`).attr("src", getAuth().ava);
+      }
+      if ($(`.nav-bar-menu .logout`).hasClass("hidden")) {
+        $(`.nav-bar-menu .logout`).removeClass("hidden");
+      }
+      if (!$(`.nav-bar-menu .login`).hasClass("hidden")) {
+        $(`.nav-bar-menu .login`).addClass("hidden");
+      }
     } else {
-        if (!$(`.nav-bar .user-wrap`).hasClass("hidden")) {
-            $(`.nav-bar .user-wrap`).addClass("hidden");
-        }
-        if (!$(`.nav-bar-menu .logout`).hasClass("hidden")) {
-            $(`.nav-bar-menu .logout`).addClass("hidden");
-        }
-        if ($(`.nav-bar-menu .login`).hasClass("hidden")) {
-            $(`.nav-bar-menu .login`).removeClass("hidden");
-        }
+      if (!$(`.nav-bar .user-wrap`).hasClass("hidden")) {
+        $(`.nav-bar .user-wrap`).addClass("hidden");
+      }
+      if (!$(`.nav-bar-menu .logout`).hasClass("hidden")) {
+        $(`.nav-bar-menu .logout`).addClass("hidden");
+      }
+      if ($(`.nav-bar-menu .login`).hasClass("hidden")) {
+        $(`.nav-bar-menu .login`).removeClass("hidden");
+      }
     }
 
     userMenuBtnBehavior();
-})
+  }, 100);
+});
